cb8cfbd206f032d9fe500cf4c8490756bb0ff613
yatharth17 <yatharthkatyal4@gmail.com>
2019-10-15 08:33:46 +0530

    Reducers

--- actions.js
+++ actions.js
@@ -1,10 +1,10 @@
 //Basic structure of an Action
 const BUY_CAKE= 'BUY_CAKE'
 //An action always must have a type property
-{
-    type:BUY_CAKE;
-    info: "First redux action"
-}
+// {
+//     type:BUY_CAKE;
+//     info: "First redux action"
+// }
 
 //An action creater
 function buyCake(){
